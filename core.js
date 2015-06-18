2d60f5bf0f9edfec421ff2413c151f752505a7b9
daniel-cole <dannycole12@gmail.com>
2015-06-18 14:38:07 +1200

    changed to register

--- core.js
+++ core.js
@@ -24,7 +24,7 @@ app.use(cors());
 
 // Auth Endpoins
 app.post('/auth/login', auth.login);
-app.post('/auth/register', auth.login);
+app.post('/auth/register', auth.register);
 
 
 // Quotes endpoints
